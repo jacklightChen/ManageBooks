0b18871a71355df045b3dd81c83a3c8d84c26c89
Zhicheng Chen <chen1319276472@foxmail.com>
2018-05-11 22:59:20 +0800

    0511

--- src/main/java/cn/lightina/managebooks/pojo/Reservation.java
+++ src/main/java/cn/lightina/managebooks/pojo/Reservation.java
@@ -1,5 +1,42 @@
 package cn.lightina.managebooks.pojo;
 
-public class Reservation {
+import java.util.Date;
 
+public class Reservation {
+    private int REID;
+    private int RID;
+    private int BID;
+    private Date deadline;
+
+    public int getREID() {
+        return REID;
+    }
+
+    public void setREID(int REID) {
+        this.REID = REID;
+    }
+
+    public int getRID() {
+        return RID;
+    }
+
+    public void setRID(int RID) {
+        this.RID = RID;
+    }
+
+    public int getBID() {
+        return BID;
+    }
+
+    public void setBID(int BID) {
+        this.BID = BID;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
 }
