79468cc44e81213e44b291f1619b9bfb642f4dde
Zhicheng Chen <chen1319276472@foxmail.com>
2018-05-14 17:12:52 +0800

    almost done

--- src/main/java/cn/lightina/managebooks/dao/MailMapper.java
+++ src/main/java/cn/lightina/managebooks/dao/MailMapper.java
@@ -6,6 +6,9 @@ import cn.lightina.managebooks.pojo.User;
 import java.util.List;
 
 public interface MailMapper {
+    // TODO: 2018/5/14 czh:写完了，测试测试
     List<MailDetail> returnReminder();
+
+    // TODO: 2018/5/14 cah:注意一下这里MailDetail.btime字段为空
     List<MailDetail> resReminder();
 }
